bb2fb4b76f35eb896f6de5e107499c9b6769d490
nldoc <jasa@posteo.de>
2019-02-05 09:42:33 +0100

    Ants genalg plot update

--- 2_UseCases/7_Ants_genAlg.R
+++ 2_UseCases/7_Ants_genAlg.R
@@ -47,17 +47,17 @@ setsim(nl, "simoutput") <- results
 # Store nl object
 saveRDS(nl, "3_Results/Ants_genAlg_nl.rds")
 
-
+### DEFINE PLOT MARGIN:
+pmarg <- 10
 
 #### Plot 1: Fitness function:
 fitness <- data.frame(generation=seq(1:length(results$mean)), evaluation=results$mean)
 
 fitness.plot <- ggplot(fitness, aes(x=generation, y=evaluation)) +
   geom_line(size=1) +
-  theme_minimal() +
-  theme(axis.text=element_text(size=14),
-        axis.title=element_text(size=14)) + 
-  theme_ipsum(axis_title_size = 14)
+  theme_ipsum(base_size = 14, axis_text_size = 14, axis_title_size = 14, strip_text_size = 14) +
+  theme(plot.margin = margin(t = pmarg, r = pmarg, b = pmarg, l = pmarg, unit = "pt")) 
+  
 
 results.summary <- strsplit(summary(results), " ")
 results.summary <- tibble(parameter = names(nl@experiment@variables),
@@ -66,7 +66,7 @@ results.summary <- tibble(parameter = names(nl@experiment@variables),
 
 fitness.best.table <- tableGrob(results.summary, rows=rep("",nrow(results.summary)))
 
-fitness.plot <- fitness.plot + annotation_custom(fitness.best.table, xmin=50, ymin=2000)
+#fitness.plot <- fitness.plot + annotation_custom(fitness.best.table, xmin=50, ymin=2000)
 
 ##################################
 ## Use best found setting to simulate one run of wolf sheep over 2000 ticks:
@@ -110,27 +110,27 @@ setsim(nl, "simoutput") <- results.best
 # Store nl object
 saveRDS(nl, "3_Results/Ants_genAlg_valid_nl.rds")
 
-
-results.best.agg <- results.best %>% dplyr::select(`random-seed`, nl@experiment@metrics)
-names(results.best.agg) <- c("seed", "step", "Foodsource 1", "Foodsource 2", "Foodsource 3")
-results.best.agg <- results.best.agg %>% 
-  gather(foodsource, food, -step, -seed) %>% 
-  dplyr::filter(food > 0)
-
-ggplot(results.best.agg, aes(x=step, y=food)) +
-  facet_wrap(~foodsource) +
-  geom_line(aes(color=factor(seed)), size=2) + 
-  guides(color="none") +
-  scale_color_jco() +
-  theme_ipsum(base_size = 14, axis_text_size = 14, axis_title_size = 14, strip_text_size = 14) +
-  theme(panel.spacing = unit(0.5, "lines"))
+# 
+# results.best.agg <- results.best %>% dplyr::select(`random-seed`, nl@experiment@metrics)
+# names(results.best.agg) <- c("seed", "step", "Foodsource 1", "Foodsource 2", "Foodsource 3")
+# results.best.agg <- results.best.agg %>% 
+#   gather(foodsource, food, -step, -seed) %>% 
+#   dplyr::filter(food > 0)
+# 
+# ggplot(results.best.agg, aes(x=step, y=food)) +
+#   facet_wrap(~foodsource) +
+#   geom_line(aes(color=factor(seed)), size=2) + 
+#   guides(color="none") +
+#   scale_color_jco() +
+#   theme_ipsum(base_size = 14, axis_text_size = 14, axis_title_size = 14, strip_text_size = 14) +
+#   theme(panel.spacing = unit(0.5, "lines"))
   
 
 #ggsave("4_Plots/Ants_sobol.png", width = 6.0, height = 4.0, dpi=300)
 
 ### Try alternate plot counting finished runs:
 results.best.agg <- results.best %>% dplyr::select(`random-seed`, nl@experiment@metrics)
-names(results.best.agg) <- c("seed", "step", "Foodsource 1", "Foodsource 2", "Foodsource 3")
+names(results.best.agg) <- c("seed", "step", "1", "2", "3")
 results.best.agg <- results.best.agg %>% 
   gather(foodsource, food, -step, -seed) %>% 
   dplyr::filter(food > 0) %>% 
@@ -140,26 +140,48 @@ results.best.agg <- results.best.agg %>%
   ungroup() %>% 
   group_by(foodsource) %>% 
   mutate(id=row_number()) %>% 
-  mutate(percentage.finished = id / length(nl@simdesign@simseeds) * 100)
+  mutate(runs.finished = id / length(nl@simdesign@simseeds) * 100)
 
 
-validplot <- ggplot(results.best.agg, aes(x=step, y=percentage.finished, color=foodsource)) +
-  geom_step(size=2) +
-  guides(color=guide_legend(title="")) +
+validplot <- ggplot(results.best.agg, aes(x=step, y=runs.finished, color=foodsource)) +
+  geom_step(size=1) +
+  guides(color=guide_legend(title="foodsource")) +
   scale_color_jco() +
-  geom_vline(xintercept = max(dplyr::filter(results.best.agg, foodsource=="Foodsource 1")$step), lty=2) +
-  geom_vline(xintercept = max(dplyr::filter(results.best.agg, foodsource=="Foodsource 2")$step), lty=2) +
-  geom_vline(xintercept = max(dplyr::filter(results.best.agg, foodsource=="Foodsource 3")$step), lty=2) +
+  geom_vline(xintercept = max(dplyr::filter(results.best.agg, foodsource=="1")$step), lty=2) +
+  geom_vline(xintercept = max(dplyr::filter(results.best.agg, foodsource=="2")$step), lty=2) +
+  geom_vline(xintercept = max(dplyr::filter(results.best.agg, foodsource=="3")$step), lty=2) +
   theme_ipsum(base_size = 14, axis_text_size = 14, axis_title_size = 14, strip_text_size = 14) +
+  ylab("runs finished [%]") +
   theme(panel.spacing = unit(0.5, "lines"),
-        legend.position = "top")
+        legend.position = "top",
+        plot.margin = margin(t = pmarg, r = pmarg, b = pmarg, l = pmarg, unit = "pt"))
+
+### Cumulative per seed:
+
+results.best.agg <- results.best %>% dplyr::select(`random-seed`, nl@experiment@metrics)
+names(results.best.agg) <- c("seed", "step", "food1", "food2", "food3")
+results.best.agg <- results.best.agg %>% 
+  mutate(foodsum=food1 + food2 + food3) %>% 
+  group_by(seed) %>% 
+  mutate(maxfood=max(foodsum)) %>% 
+  mutate(percentage.eaten=round(((maxfood - foodsum) / maxfood) * 100, digits=3)) %>% 
+  mutate(maxstep=max(step))
+
+cumplot <- ggplot(results.best.agg, aes(x=step, y=percentage.eaten, color=factor(seed))) +
+  geom_step(size=1) +
+  guides(color="none") +
+  scale_color_jco() +
+  ylab("food gathered [%]") +
+  geom_vline(aes(xintercept = maxstep, color=factor(seed)), lty=2) +
+  theme_ipsum(base_size = 14, axis_text_size = 14, axis_title_size = 14, strip_text_size = 14) +
+  theme(plot.margin = margin(t = pmarg, r = pmarg, b = pmarg, l = pmarg, unit = "pt"))
 
 
-plots <- list(fitness.plot, validplot)
-lay <- rbind(c(1,2)) 
+
+
+plots <- list(fitness.plot, fitness.best.table, cumplot, validplot)
+lay <- rbind(c(1,2), c(3,4)) 
 grid.arrange(grobs=plots, layout_matrix=lay)
 
-ggsave("4_Plots/Ants_genalg.png", width=10, height=3, arrangeGrob(grobs=plots, layout_matrix=lay))  
-
-
+ggsave("4_Plots/Ants_genalg.png", width=7, height=6, arrangeGrob(grobs=plots, layout_matrix=lay))  
 
