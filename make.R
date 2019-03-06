4a3c03a8cdfc5936dbf91c7ecb7c48602a2c067a
nldoc <jasa@posteo.de>
2019-03-07 10:54:56 +1300

    usecase cleanup - new manuscript version

--- make.R
+++ make.R
@@ -3,8 +3,25 @@
 ## source globals
 source("1_Helper/globals.R")
 
-## run eFast use case
-source("2_UseCases/1_WolfSheep_eFast.R")
+## run sobol use case
+source("2_UseCases/1_Ants_sobolt.R")
 
 ## run GenAlg use case
-source("2_UseCases/2_WolfSheep_GenAlg.R")
+source("2_UseCases/2_Ants_genAlg.R")
+
+## run point pattern analysis use case
+source("2_UseCases/3_Ants_spatstat.R")
+
+## run screenshot use case
+source("2_UseCases/4_Ants_screenshot.R")
+
+## run benchmark
+#source("2_UseCases/5_1_run_benchmarks_unix.R")
+#source("2_UseCases/5_1_run_benchmarks_win.R")
+
+## run benchmark plot script
+source("2_UseCases/5_2_Plot_Benchmarks.R")
+
+## run nldoc use case
+source("2_UseCases/6_nldoc.R")
+
